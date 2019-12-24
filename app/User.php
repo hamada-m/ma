ae61fc74b7f13ccfdaf040cb549d6bd51a915936
Mohammed ZOUITNI <stgr2.wetic@gmail.com>
2019-12-24 16:54:15 +0100

    Generate Question model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -6,33 +6,41 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
+    public function questions() {
+        return $this->hasMany( Qustion:: class );
+    }
+
+    public function setTitleAttribute( $value ) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug( $value );
+    }
+
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    * The attributes that should be cast to native types.
+    *
+    * @var array
+    */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
