c9a476e54798ad2dc22b0405445b15927a27f383
Mohammed ZOUITNI <stgr2.wetic@gmail.com>
2020-02-05 12:38:59 +0100

    Implement authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller {
+
+    public function __construct()
+ {
+        $this->middleware( 'auth', ['except' =>['index', 'show']] );
+
+    }
+
     /**
     * Display a listing of the resource.
     *
@@ -64,6 +71,7 @@ class QuestionsController extends Controller {
     */
 
     public function edit( Question $question ) {
+        $this->authorize( 'update', $question );
         return view( 'questions.edit', compact( 'question' ) );
     }
 
@@ -76,6 +84,8 @@ class QuestionsController extends Controller {
     */
 
     public function update( AskQuestionRequest $request, Question $question ) {
+        $this->authorize( 'update', $question );
+
         $question->update( $request->only( 'title', 'body' ) );
         return redirect( '/questions' )->with( 'success', 'Your question has been updated.' );
     }
@@ -88,6 +98,8 @@ class QuestionsController extends Controller {
     */
 
     public function destroy( Question $question ) {
+        $this->authorize( 'delete', $question );
+
         $question->delete();
         return redirect( '/questions' )->with( 'success', 'Your question has been deleted.' );
 
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,25 +4,28 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Question;
+use App\Policies\QuestionPolicy;
 
 class AuthServiceProvider extends ServiceProvider
-{
+ {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
+    * The policy mappings for the application.
+    *
+    * @var array
+    */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        Question::class => QuestionPolicy::class,
     ];
 
     /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
+    * Register any authentication / authorization services.
+    *
+    * @return void
+    */
+
     public function boot()
-    {
+ {
         $this->registerPolicies();
 
         //
